53148321128cfa19c93457d559f455e058eeb32c
Simon Ouellet <souellet@protect-yu.com>
2020-06-23 21:21:50 -0400

    Updates

---
+++ precache-manifest.5f8b818fae9124d9a0aa65ee679ea72e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f9c509959cf6e235bd56cb38ccba5c0",
+    "url": "/zelda-botw-starter/index.html"
+  },
+  {
+    "revision": "9c2f0c6ff29d914f6995",
+    "url": "/zelda-botw-starter/static/css/main.fd1968fe.chunk.css"
+  },
+  {
+    "revision": "fdcd63cfb049cd555328",
+    "url": "/zelda-botw-starter/static/js/2.16547c78.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/zelda-botw-starter/static/js/2.16547c78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c2f0c6ff29d914f6995",
+    "url": "/zelda-botw-starter/static/js/main.e64ff212.chunk.js"
+  },
+  {
+    "revision": "dc3383e8914e42edcefd",
+    "url": "/zelda-botw-starter/static/js/runtime-main.cfebe4ee.js"
+  },
+  {
+    "revision": "51d61b62d86632c9a08900d5c448f4e4",
+    "url": "/zelda-botw-starter/static/media/triforce.51d61b62.svg"
+  }
+]);
