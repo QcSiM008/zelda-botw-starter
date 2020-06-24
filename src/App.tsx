837d6e8e23bb9a11db14e3a4af67cee3a76c7658
Simon Ouellet <souellet@protect-yu.com>
2020-06-23 21:46:57 -0400

    switched to hash router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Menu from './pages/Menu';
 
 const App = () => (
   <div className="min-h-screen flex justify-center items-center">
-    <Router>
+    <Router basename="/">
       <Switch>
         <Route exact path="/">
           <Home />
